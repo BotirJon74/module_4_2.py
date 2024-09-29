eb9349277c2cdf4a6ff4cd8a1c6142ebed9b08c3
BotirJon74 <31174@mail.ru>
2024-09-29 10:55:23 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -4,6 +4,9 @@ def test_function():
     inner_function()
 
 test_function()
-inner_function()
+# inner_function()        File "D:\pythonProject\variable\module_4_2.py", line 7, in <module>
+#                             inner_function()
+#                              ^^^^^^^^^^^^^^
+#                         NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
 
 
